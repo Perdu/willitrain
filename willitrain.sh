ab2be1034383658e72009794e900452781cfd54a
Célestin Matte <celestin.matte@gmail.com>
2016-05-22 10:38:09 +0200

    Bugfix: correctly grep rain when there's a storm

--- willitrain.sh
+++ willitrain.sh
@@ -2,7 +2,7 @@
 
 weather=$(wget -O - http://www.accuweather.com/fr/fr/villeurbanne/136558/daily-weather-forecast/136558?day=1 2>/dev/null)
 precipitations=$(echo "$weather" | grep -m1 'cipitations:' | perl -pe "s/.*>(\d+) mm.*/\1/")
-rain=$(echo "$weather" | grep -m1 Pluie | perl -pe "s/.*>(\d+) mm.*/\1/")
+rain=$(echo "$weather" | grep -m1 'Pluie:' | perl -pe "s/.*>(\d+) mm.*/\1/")
 snow=$(echo "$weather" | grep -m1 Neige | perl -pe "s/.*>(\d+) cm.*/\1/")
 ice=$(echo "$weather" | grep -m1 Verglas | perl -pe "s/.*>(\d+) mm.*/\1/")
 
