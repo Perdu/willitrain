8c3cd1b8a9b57c73485683179f50cb827b8599ba
Célestin Matte <celestin.matte@gmail.com>
2020-07-11 13:05:52 +0200

    Update to match latest weather.com update

--- willitrain.sh
+++ willitrain.sh
@@ -3,14 +3,23 @@
 
 #USER_AGENT='Mozilla/5.0 Firefox/60.0'
 weather=$(wget -O - 'https://weather.com/fr-FR/temps/aujour/l/45.78,4.86?par=google' 2>/dev/null)
-precipitations=$(echo "$weather" | grep -oP "\"precipPct\":\d+" | head -1 | perl -pe "s/.*:(\d+)/\1/")
+precipitations=$(echo "$weather" | grep -oPm4 "precip--3JCDO\">\d+%</span>" | perl -pe "s/.*>(\d+)%<.*/\1/")
 rain=0 #$(echo "$weather" | grep -m1 'Pluie:' | perl -pe "s/.*>(\d+) mm.*/\1/")
 snow="0" #$(echo "$weather" | grep -m1 Neige | perl -pe "s/.*>((\d*.)?\d+) cm.*/\1/")
 ice="0" #$(echo "$weather" | grep -m1 Verglas | perl -pe "s/.*>((\d*.)?\d+) mm.*/\1/")
 
-if [ "$precipitations" -gt 0 ]
+max=0
+for p in $precipitations
+do
+    if [ "$p" -gt "$max" ]
+    then
+        max=$p
+    fi
+done
+
+if [ "$max" -gt 0 ]
 then
-    echo "Précipitations : $precipitations%"
+    echo "Précipitations : $max%"
 fi
 
 #if [ "$rain" -gt 0 ]
@@ -28,7 +37,7 @@ fi
 #    echo "Verglas : $ice mm"
 #fi
 
-if [ "$precipitations" -gt 0 -o "$rain" -gt 0 -o "$snow" != "0" -o "$ice" -gt 0 ]
+if [ "$max" -gt 0 -o "$rain" -gt 0 -o "$snow" != "0" -o "$ice" -gt 0 ]
 then
     echo 'https://www.google.fr/search?q=météo'
 fi
